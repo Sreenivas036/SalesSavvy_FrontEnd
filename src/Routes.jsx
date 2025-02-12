7f44d80aaadb970e914c0d42a106dca86897cafc
Sreenivas036 <159412763+Sreenivas036@users.noreply.github.com>
2025-02-12 09:02:46 +0530

    12-02-2025
    updated

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -3,10 +3,10 @@ import { Routes, Route } from "react-router-dom";
 import LoginPage from "./LoginPage";
 import RegistrationPage from "./RegistrationPage";
  import CustomerHomePage from "./CustomerHomePage";
-import CartPage from "./CartPage";
- import OrderPage from "./OrderPage";
- import AdminLogin from "./AdminLogin"; 
- import AdminDashboard from "./AdminDashboard";
+// import CartPage from "./CartPage";
+//  import OrderPage from "./OrderPage";
+//  import AdminLogin from "./AdminLogin"; 
+//  import AdminDashboard from "./AdminDashboard";
 
 const AppRoutes = () => {
   return (
@@ -14,10 +14,10 @@ const AppRoutes = () => {
       <Route path="/" element={<LoginPage />} />
       <Route path="/register" element={<RegistrationPage />} />
      <Route path="/customerhome" element={<CustomerHomePage />} />
-      <Route path="/UserCartPage" element={<CartPage />} />
-      <Route path="/orders" element={<OrderPage />} />
-      <Route path="/admin" element={<AdminLogin />} />
-      <Route path="/admindashboard" element={<AdminDashboard />} /> 
+      {/* <Route path="/UserCartPage" element={<CartPage />} /> */}
+      {/* <Route path="/orders" element={<OrderPage />} /> */}
+      {/* <Route path="/admin" element={<AdminLogin />} /> */}
+      {/* <Route path="/admindashboard" element={<AdminDashboard />} />  */}
     </Routes>
   );
 };
